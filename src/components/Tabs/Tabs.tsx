86bc5ae31b7d68e394d1ad879c086ba5784a7e56
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-08-21 09:26:29 +0900

    feat: create TabsProvider

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,14 +1,15 @@
-import { FC, ReactNode, useState } from 'react'
+import { FC, ReactNode } from 'react'
 import styles from './Tabs.module.scss'
+import { TabsProvider } from './TabsProvider'
 
 interface TabsProps {
-  children: ReactNode
+  children: ReactNode[]
 }
 
 export const Tabs: FC<TabsProps> = ({ children }) => {
-  const [selectedTab, setSelectedTab] = useState(0)
-
-  console.log({children})
-
-  return <div className={styles.root}>{children}</div>
+  return (
+    <TabsProvider>
+      <div className={styles.root}>{children}</div>
+    </TabsProvider>
+  )
 }
