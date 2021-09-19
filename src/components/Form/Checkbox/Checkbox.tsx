3a8edb72f8955bbb76150072004e4ed8e6272fd7
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-09-19 23:39:28 +0900

    fix: Checkboxを修正

--- src/components/Form/Checkbox/Checkbox.tsx
+++ src/components/Form/Checkbox/Checkbox.tsx
@@ -25,7 +25,7 @@ export const Checkbox: FC<CheckboxProps> = ({
         tabIndex={-1} // タブでフォーカスさせない
         className={styles.input}
       />
-      <span role={'checkbox'} tabIndex={0} className={styles.checkmark}></span>
+      <span className={styles.checkmark}></span>
       <span className={styles.label}>{label}</span>
     </label>
   )
