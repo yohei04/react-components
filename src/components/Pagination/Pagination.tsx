04cee44d96a8943c2d882d037932c0720b3a719b
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-09-28 17:52:42 +0900

    feat: データを追加

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,16 +1,15 @@
-import { FC, ReactNode, useState } from 'react'
+import { FC, useState } from 'react'
 import { PaginationItem } from '.'
 import styles from './Pagination.module.scss'
 
 interface PaginationProps {
-  children: ReactNode
-  count: number
+  totalPageCount: number
 }
 
-export const Pagination: FC<PaginationProps> = ({ count }) => {
+export const Pagination: FC<PaginationProps> = ({ totalPageCount }) => {
   const [currentPage, setCurrentPage] = useState(1)
   const handleCurrentPage = (page: number) => setCurrentPage(page)
-  const pages = Array.from({ length: count }, (_, i) => i + 1)
+  const pages = Array.from({ length: totalPageCount }, (_, i) => i + 1)
 
   return (
     <nav aria-label="pagination">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,34 @@
+import { Pagination } from '@components/Pagination'
+import { Spacer } from '@components/Spacer'
+import utilStyles from 'styles/base/Utils.module.scss'
+
 export const Home = (): JSX.Element => {
-  return <div className="container"></div>
+  /** トータルの data の数 */
+  const totalCount = 21
+  /** 1ページに表示する数 */
+  const pageSize = 5
+  /** トータルのページ数 */
+  const totalPageCount = Math.ceil(totalCount / pageSize)
+  const posts = [...Array(totalCount)].map((e, i) => {
+    return { id: i + 1, title: `post-${i + 1}`, content: `content-${i + 1}` }
+  })
+
+  return (
+    <div>
+      <div
+        style={{ display: 'flex', flexWrap: 'wrap' }}
+        className={utilStyles.p_10}
+      >
+        {posts.map(({ id, title, content }) => (
+          <div key={id} style={{ display: 'flex' }} className={utilStyles.p_1}>
+            <h3>{title}</h3>
+          </div>
+        ))}
+      </div>
+      {/* <Spacer mt={3} /> */}
+      <Pagination totalPageCount={totalPageCount} />
+    </div>
+  )
 }
 
 export default Home
