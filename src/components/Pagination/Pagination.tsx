fabd81c1ff2bea8492a98acfcd6888afb9eb889a
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-09-29 03:08:31 +0900

    fix: データ表示部分を修正

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,14 +1,18 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import { PaginationFirst, PaginationItem, PaginationPrev } from '.'
 import styles from './Pagination.module.scss'
 
 interface PaginationProps {
   totalPageCount: number
+  currentPage: number
+  handleCurrentPage: (page: number) => void
 }
 
-export const Pagination: FC<PaginationProps> = ({ totalPageCount }) => {
-  const [currentPage, setCurrentPage] = useState(1)
-  const handleCurrentPage = (page: number) => setCurrentPage(page)
+export const Pagination: FC<PaginationProps> = ({
+  totalPageCount,
+  currentPage,
+  handleCurrentPage,
+}) => {
   const pages = Array.from({ length: totalPageCount }, (_, i) => i + 1)
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
 import { Pagination } from '@components/Pagination'
-import { Spacer } from '@components/Spacer'
+import { useState } from 'react'
 import utilStyles from 'styles/base/Utils.module.scss'
 
 export const Home = (): JSX.Element => {
-  /** トータルの data の数 */
-  const totalCount = 21
+  const posts = [...Array(21)].map((e, i) => {
+    return { id: i + 1, title: `post-${i + 1}`, content: `content-${i + 1}` }
+  })
+
+  /** トータルのデータ数 */
+  const totalCount = posts.length
   /** 1ページに表示する数 */
   const pageSize = 5
   /** トータルのページ数 */
   const totalPageCount = Math.ceil(totalCount / pageSize)
-  const posts = [...Array(totalCount)].map((e, i) => {
-    return { id: i + 1, title: `post-${i + 1}`, content: `content-${i + 1}` }
-  })
+  const [currentPage, setCurrentPage] = useState(1)
+  const handleCurrentPage = (page: number) => setCurrentPage(page)
 
   return (
     <div>
@@ -19,14 +22,26 @@ export const Home = (): JSX.Element => {
         style={{ display: 'flex', flexWrap: 'wrap' }}
         className={utilStyles.p_10}
       >
-        {posts.map(({ id, title, content }) => (
-          <div key={id} style={{ display: 'flex' }} className={utilStyles.p_1}>
-            <h3>{title}</h3>
-          </div>
-        ))}
+        {posts
+          .slice(
+            (currentPage - 1) * pageSize,
+            (currentPage - 1) * pageSize + pageSize
+          )
+          .map(({ id, title }) => (
+            <div
+              key={id}
+              style={{ display: 'flex' }}
+              className={utilStyles.p_1}
+            >
+              <h3>{title}</h3>
+            </div>
+          ))}
       </div>
-      {/* <Spacer mt={3} /> */}
-      <Pagination totalPageCount={totalPageCount} />
+      <Pagination
+        totalPageCount={totalPageCount}
+        currentPage={currentPage}
+        handleCurrentPage={handleCurrentPage}
+      />
     </div>
   )
 }
