45a6b99087a582255624693b6e53e2111caa6896
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-12-12 07:42:52 +0900

    feat: ドットが無い場合を追加

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -24,10 +24,22 @@ export const Pagination: FC<PaginationProps> = ({
 
   /** 現在のページから左右何ページ表示するかの数 */
   const siblingCount = 2
+  // siblingCount + firstPage + lastPage + currentPage + 2*DOTS。 (firstPage + lastPageは別コンポーネントのため含まない)
+  const totalPageNumbers = siblingCount + 3
 
   const pager = pages.slice(1, totalPageCount - 1).map((page) => {
-    // 右側にのみドットがある場合
-    if (currentPage <= siblingCount + 3) {
+    // ドットがない場合
+    if (totalPageNumbers >= totalPageCount) {
+      return (
+        <PaginationItem
+          key={page}
+          page={page}
+          currentPage={currentPage}
+          handleCurrentPage={handleCurrentPage}
+        />
+      )
+      // 右側にのみドットがある場合
+    } else if (currentPage <= siblingCount + 3) {
       // 最後から2番目にドットを表示
       if (page === totalPageCount - 2) {
         return <PaginationDots key={page} />
